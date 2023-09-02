22852e0460cf5985d58ffb609c63bd5cbc4dd621
slipanchor578 <slipanchor578@gmail.com>
2023-09-02 11:26:34 +0900

    First Commit

--- service-workers/index.js
+++ service-workers/index.js
@@ -1,38 +1,14 @@
 window.addEventListener("load", async () => {
-    if ("serviceWorker" in navigator) {
-        try {
-            const sw = await navigator.serviceWorker.register("sw.js", { scope: "./" });
-            document.getElementById("status").innerHTML = "successful";
-            let serviceWorker = null;
-            if (sw.installing) {
-                serviceWorker = sw.installing;
-                printState("installing");
-            }
-            else if (sw.waiting) {
-                serviceWorker = sw.waiting;
-                printState("waiting");
-            }
-            else if (sw.active) {
-                serviceWorker = sw.active;
-                printState("active");
-            }
-            if (serviceWorker) {
-                printState(serviceWorker.state);
-                serviceWorker.addEventListener("statechange", (e) => {
-                    printState(e.target.state);
-                });
-            }
-        }
-        catch (err) {
-            if (err instanceof Error) {
-                document.getElementById("status").innerHTML = err.message;
-            }
-        }
-    }
-    else {
-        document.getElementById("status").innerHTML = "unavailable";
-    }
-    function printState(state) {
-        document.getElementById("state").innerHTML = state;
-    }
+    const controllerChangePromise = new Promise(resolve => {
+        navigator.serviceWorker.addEventListener("controllerchange", resolve);
+    });
+    navigator.serviceWorker.register("./sw.js", { scope: "./" })
+        .then((registration) => {
+        if (navigator.serviceWorker.controller)
+            return;
+        return controllerChangePromise;
+    })
+        .then(() => {
+        console.log(navigator.serviceWorker.controller !== null);
+    });
 }, { capture: false, once: true, passive: true });
--- service-workers/sw.js
+++ service-workers/sw.js
@@ -1,6 +1,44 @@
-self.addEventListener("install", (e) => {
-    console.log(`Install Event: ${e}`);
+const CACHENAME = "myCache";
+const CACHEVERSION = 1;
+const CACHEKEY = `${CACHENAME}:${CACHEVERSION}`;
+const CACHEFILES = [
+    "./index.html",
+    "./index.js",
+    "./index.css"
+].map(path => new URL(path, self.registration.scope).pathname);
+function isTargetFile(url) {
+    return CACHEFILES.indexOf(new URL(url).pathname) >= 0;
+}
+async function installHandler() {
+    const cache = await caches.open(CACHEKEY);
+    return cache.addAll(CACHEFILES);
+}
+async function activatehandler() {
+    const cacheList = await caches.keys();
+    const activeCacheList = cacheList.map((cacheName) => {
+        if (!CACHEKEY.includes(cacheName)) {
+            return caches.delete(cacheName);
+        }
+    });
+    return Promise.all([...activeCacheList, self.clients.claim()]);
+}
+async function fetchHandler(event) {
+    const cache = await caches.open(CACHEKEY);
+    const cachedContents = await cache.match(event.request);
+    if (cachedContents)
+        return cachedContents;
+    const response = await fetch(event.request);
+    if (isTargetFile(event.request.url) && response.ok) {
+        cache.put(event.request, response.clone());
+    }
+    return response;
+}
+self.addEventListener("install", (event) => {
+    event.waitUntil(installHandler());
 });
-self.addEventListener("activate", (e) => {
-    console.log(`Activate Event: ${e}`);
+self.addEventListener("activate", async (event) => {
+    event.waitUntil(activatehandler());
+});
+self.addEventListener("fetch", (event) => {
+    event.respondWith(fetchHandler(event));
 });
